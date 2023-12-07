9b97507d509d8bdba7bdd5fb40058fcc6acb8968
Alex Petreti <alex.petreti@studio.unibo.it>
2023-12-07 16:56:00 +0100

    implemented controller

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile;
 
+    public Controller() {
+        String userHome = System.getProperty("user.home");
+        String fileSeparator = System.getProperty("file.separator");
+        String defaultFilePath = userHome + fileSeparator + "output.txt";
+        this.currentFile = new File(defaultFilePath);
+    }
+
+    public void setCurrentFile(File file) {
+        this.currentFile = file;
+    }
+
+    public File getFile() {
+        return currentFile;
+    }
+
+    public String getCurrentPath() {
+        return currentFile.getPath();
+    }
+
+    public void inputString(String string) throws IOException {
+        try {
+            FileWriter writer = new FileWriter(currentFile);
+            writer.write(string);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("No file is currently set");
+        }
+    }
 }
